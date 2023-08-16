1bee84edf24bd329333942f675c4ceb67726f692
Sanjay <sanjaylavel@gmail.com>
2023-08-16 20:08:24 +0530

    changed linux

--- setup.sh
+++ setup.sh
@@ -7,12 +7,17 @@ isPackageNotInstalled() {
     if [ $? -eq 0 ]; then
         echo "$1: Already installed"
     else
+        sudo apt-get update
         sudo apt-get install -y $1
     fi
 }
 
 sdl=libsdl2-dev
+make=make
+cc=gcc
 
+isPackageNotInstalled $cc
+isPackageNotInstalled $make
 isPackageNotInstalled $sdl
 
 ./premake5 gmake2;
